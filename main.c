06b6f6072f41b3bef45b3692010c72b147235b24
Gabriella Ponce <gabipaz04@gmail.com>
2025-09-27 17:26:34 -0300

    automato de id atualizado

--- main.c
+++ main.c
@@ -133,42 +133,13 @@ Token proximo_token() {
               estado = ESTADO_INICIAL;
               cont_sim_lido++;
             }
+            
+            //ids e palavras reservadas(verificadas dentro de ID)
 
-            //palavras reservadas
-
-            else if (c == 'i') {
-                estado = BEGIN;
-                cont_sim_lido++;
+            else if (isalpha(c)){
+                estado = ID;
             }
-            else if (c == 'f') {
-                estado = END;
-                cont_sim_lido++;
-            }
-            else if (c == 'e') {
-                estado = ELSE;
-                cont_sim_lido++;
-            }
-            else if (c == 'w') {
-              estado = WHILE;
-              cont_sim_lido++;
-            }
-            else if (c == 'r') {
-              estado = READ;
-              cont_sim_lido++;
-            }
-            else if (c == 'p'){
-              estado = PRINT;
-              cont_sim_lido++;
-            }
-            else if (c == 'f') {
-              estado = TYPE_FLOAT;
-              cont_sim_lido++;
-            }
-            else if (c == 's') {
-              estado = TYPE_STRING;
-              cont_sim_lido++;
-            }
-
+            
             //caracteres únicos
 
             else if (c == ';') {
@@ -207,68 +178,82 @@ Token proximo_token() {
             else 
               estado = falhar();
             break;
+      
+      case ID:
+            char lexema[30];
+            int pos = 0;
+            
+            lexema[pos] = c;
+            cont_sim_lido++;
+            pos++;
+            
+            while (cont_sim_lido < strlen(code) && code[cont_sim_lido] != '\0' && pos < 99){
+              
+                c = code[cont_sim_lido];
 
-      case BEGIN:
-            c = code[cont_sim_lido];
-            if (c == 'n'){
-              estado = 1;
-              cont_sim_lido++;
+                if (c == ' ' || c == '\n' || (!isalnum(c) && c != '_')){
+                  break;
+                }
+                
+                lexema[pos] = c;
+                pos++;
+                cont_sim_lido++;
+                
             }
-            else if (c == 'f'){
+
+            lexema[pos] = '\0';
+
+            if(strcmp(lexema, "inicio") == 0){
+              estado = BEGIN;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "fim") == 0){
+              estado = END;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "if") == 0){
               estado = IF;
+              cont_sim_lido--;
             }
-            else{
-              estado = ESTADO_INICIAL;
+            else if(strcmp(lexema, "else") == 0){
+              estado = ELSE;
+              cont_sim_lido--;
             }
-            break;
-
-        case 1:
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              estado = 2;
-              cont_sim_lido++;
+            else if(strcmp(lexema, "while") == 0){
+              estado = WHILE;
+              cont_sim_lido--;
             }
-            else if (c == 't'){
+            else if(strcmp(lexema, "read") == 0){
+              estado = READ;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "print") == 0){
+              estado = PRINT;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "int") == 0){
               estado = TYPE_INT;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "float") == 0){
+              estado = TYPE_FLOAT;
+              cont_sim_lido--;
+            }
+            else if(strcmp(lexema, "string") == 0){
+              estado = TYPE_STRING;
+              cont_sim_lido--;
             }
             else{
-              estado = BEGIN;
-            }
-            break;
-
-        case 2:
-            c = code[cont_sim_lido];
-            if (c == 'c'){
-              estado = 3;
-              cont_sim_lido++;
-            }
-            else{
-              estado = BEGIN;
-            }
-            break;
-  
-        case 3:
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              estado = 4;
-              cont_sim_lido++;
-            }
-            else{
-              estado = BEGIN;
-            }
-            break;
-        
-        case 4:
-            c = code[cont_sim_lido];
-            if (c == 'o'){
-              estado = 5;
-            }
-            else{
+              printf("<ID, >\n");
+              token.nome_token = ID;
+              token.atributo = -1;
               estado = ESTADO_INICIAL;
+              return(token);
             }
-            break;
 
-        case 5:
+        break;
+
+        case BEGIN:
             cont_sim_lido++;
             printf("<inicio, >\n");
             token.nome_token = BEGIN;
@@ -278,34 +263,8 @@ Token proximo_token() {
             break;
         
         case END: 
-
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              estado = 7;
-              cont_sim_lido++;
-            }
-            else if (c == 'l'){
-              estado = TYPE_FLOAT;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 7:
-            c = code[cont_sim_lido];
-            if (c == 'm'){
-              estado = 8;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 8:
             cont_sim_lido++;
-            printf("<end, >\n");
+            printf("<fim, >\n");
             token.nome_token = END;
             token.atributo = -1;
             estado = ESTADO_INICIAL;
@@ -331,38 +290,6 @@ Token proximo_token() {
             break;
 
         case ELSE:
-            c = code[cont_sim_lido];
-            if (c == 'l'){
-              estado = 9;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 9:
-            c = code[cont_sim_lido];
-            if (c == 's'){
-              estado = 10;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 10:
-            c = code[cont_sim_lido];
-            if (c == 'e'){
-              estado = 11;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-          
-        case 11: 
             cont_sim_lido++;
             printf("<else, >\n");
             token.nome_token = ELSE;
@@ -372,49 +299,6 @@ Token proximo_token() {
             break;
         
         case WHILE:
-            c = code[cont_sim_lido];
-            if (c == 'h'){
-              estado = 12;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 12:
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              estado = 13;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 13:
-            c = code[cont_sim_lido];
-            if (c == 'l'){
-              estado = 14;
-              cont_sim_lido++;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 14:
-            c = code[cont_sim_lido];
-            if (c == 'e'){
-              estado = 15;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 15: 
             cont_sim_lido++;
             printf("<while, >\n");
             token.nome_token = WHILE;
@@ -424,38 +308,6 @@ Token proximo_token() {
             break;
 
         case READ:
-            c = code[cont_sim_lido];
-            if (c == 'e'){
-              cont_sim_lido++;
-              estado = 16;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 16:
-            c = code[cont_sim_lido];
-            if (c == 'a'){
-              cont_sim_lido++;
-              estado = 17;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 17:
-            c = code[cont_sim_lido];
-            if (c == 'd'){
-              estado = 18;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 18: 
             cont_sim_lido++;
             printf("<read, >\n");
             token.nome_token = READ;
@@ -465,49 +317,6 @@ Token proximo_token() {
             break;
         
         case PRINT:
-            c = code[cont_sim_lido];
-            if (c == 'r'){
-              cont_sim_lido++;
-              estado = 19;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 19:
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              cont_sim_lido++;
-              estado = 20;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 20:
-            c = code[cont_sim_lido];
-            if (c == 'n'){
-              cont_sim_lido++;
-              estado = 21;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 21:
-            c = code[cont_sim_lido];
-            if (c == 't'){
-              estado = 22;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 22: 
             cont_sim_lido++;
             printf("<print, >\n");
             token.nome_token = PRINT;
@@ -517,38 +326,6 @@ Token proximo_token() {
             break;
         
         case TYPE_FLOAT:
-            c = code[cont_sim_lido];
-            if (c == 'o'){
-              cont_sim_lido++;
-              estado = 23;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 23:
-            c = code[cont_sim_lido];
-            if (c == 'a'){
-              cont_sim_lido++;
-              estado = 24;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 24:
-            c = code[cont_sim_lido];
-            if (c == 't'){
-              estado = 25;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 25: 
             cont_sim_lido++;
             printf("<float, >\n");
             token.nome_token = TYPE_FLOAT;
@@ -558,60 +335,6 @@ Token proximo_token() {
             break;
 
         case TYPE_STRING:
-            c = code[cont_sim_lido];
-            if (c == 't'){
-              cont_sim_lido++;
-              estado = 26;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 26:
-            c = code[cont_sim_lido];
-            if (c == 'r'){
-              cont_sim_lido++;
-              estado = 27;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 27:
-            c = code[cont_sim_lido];
-            if (c == 'i'){
-              cont_sim_lido++;
-              estado = 28;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 28:
-            c = code[cont_sim_lido];
-            if (c == 'n'){
-              cont_sim_lido++;
-              estado = 29;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-
-        case 29:
-            c = code[cont_sim_lido];
-            if (c == 'g'){
-              estado = 30;
-            }
-            else{
-              estado = ESTADO_INICIAL;
-            }
-            break;
-        
-        case 30: 
             cont_sim_lido++;
             printf("<string, >\n");
             token.nome_token = TYPE_STRING;
